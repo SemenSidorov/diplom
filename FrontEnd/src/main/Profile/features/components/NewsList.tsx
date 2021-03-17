89841a6a8f5dae3dbfc4cde085326cd3a207fb35
German Slautin <sl.gera@mail.ru>
2021-03-17 22:58:11 +0300

    news page adaptive

--- FrontEnd/src/main/Profile/features/components/NewsList.tsx
+++ FrontEnd/src/main/Profile/features/components/NewsList.tsx
@@ -4,11 +4,35 @@ import {Col, Container} from "react-bootstrap";
 const NewsList = () => {
     return (
         <div style={{backgroundColor: '#ebedf0', height: "100%", width: "100%",  overflow: "auto"}}>
-            <Container style={{display: 'flex', flexDirection: 'column'}}>
+            <Container style={{display: 'flex'}}>
+                {
+                    window.screen.width > 1025 && <Col md={2}>
+                        <div>
+                            <div>
+                                Моя страница
+                            </div>
+                            <div>
+                                Новости
+                            </div>
+                            <div>
+                               Сообщеия
+                            </div>
+                            <div>
+                                Меропрития
+                            </div>
+                            <div>
+                                Настройки
+                            </div>
+                        </div>
+                    </Col>
+                }
+                <Col xl={10} xs={12}>
                     <New />
                     <New />
                     <New />
                     <New />
+                </Col>
+
             </Container>
         </div>
     );
