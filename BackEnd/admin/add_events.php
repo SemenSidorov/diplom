5786e12003a15a841d9fe4d67e2714b3b5746aa7
SemenSidorov <62383377+SemenSidorov@users.noreply.github.com>
2021-05-27 23:08:53 +0300

    lol

--- BackEnd/admin/add_events.php
+++ BackEnd/admin/add_events.php
@@ -4,7 +4,8 @@ $db = new DB;
 $name = $_GET["NAME"];
 $preview_text = $_GET["PREVIEW_TEXT"];
 $detail_text = $_GET["DETAIL_TEXT"];
-$time = time();
+$date_start = $_GET["DATE_START"];
+$date_exp = $_GET["DATE_EXT"];
 $hash = hash('ripemd160', $name);
 
 if(!empty($_FILES["PREVIEW_PICTURE"]) && exif_imagetype($_FILES['PREVIEW_PICTURE']['tmp_name']) && $_FILES["PREVIEW_PICTURE"]["error"] === 0){
@@ -20,27 +21,11 @@ $result = $db->Add("elements", [
     "PREVIEW_PICTURE" => $destination, 
     "PREVIEW_TEXT" => $preview_text, 
     "DETAIL_TEXT" => $detail_text,
-    "DATE_START" => $time
+    "DATE_START" => $date_start,
+    "DATE_EXT" => $date_exp
 ]);
 
 if(!(int)$result){
     die(json_encode($result));
-}
-
-foreach($_FILES["ADD_PICTURES"]["error"] as $key => $error){
-    if(!$error && exif_imagetype($_FILES['ADD_PICTURES']['tmp_name'][$key])){
-        $destination = $_SERVER['DOCUMENT_ROOT'] . '/BackEnd/include/img/news/' . $hash . $time . "/" . $_FILES["ADD_PICTURES"]["name"][$key];
-        if(!rename($_FILES['ADD_PICTURES']['tmp_name'][$key], $destination)){
-            die(json_encode(["ERROR" => "Ошибка добавления файла"]));
-        }
-        $res = $db->Add("elements", [
-            "PARENT_ID" => $result, 
-            "NAME" => 'ADD_PICTURES', 
-            "VALUE" => $destination
-        ]);
-        if(!(int)$res){
-            die(json_encode($res));
-        }
-    }
 }
 echo json_encode(["EVENT_ID" => $result]);
