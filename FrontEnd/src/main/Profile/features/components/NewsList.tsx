db7c4865252c21029351e551af36d2e5da43ccf6
German Slautin <sl.gera@mail.ru>
2021-06-06 18:02:48 +0300

    починил новости и подвязал мероприятия

--- FrontEnd/src/main/Profile/features/components/NewsList.tsx
+++ FrontEnd/src/main/Profile/features/components/NewsList.tsx
@@ -4,6 +4,7 @@ import {Col, Container} from "react-bootstrap";
 import { useAsync } from "@umijs/hooks";
 import {UserTypes} from "../../../Constants";
 import {useParams} from "react-router-dom";
+import {getCookieByName} from "../../../Auth/Login";
 
 export interface NewI {
     ID: string
@@ -18,14 +19,15 @@ interface NewsListI {
     values: Array<NewI>
 }
 //todo перенести в папку с методами
-const getNews = (userId: string): Promise<NewsListI> => {
-    return fetch(`http://backend/BackEnd/news/news.php?PAGEN=1&USER_ID=${userId}`).then(res => res.json());
+const getNews = (userId: string, token: any): Promise<NewsListI> => {
+    return fetch(`http://backend/BackEnd/news/news.php?TOKEN=${token}PAGEN=1&USER_ID=${userId}`).then(res => res.json());
 };
 
 
 const NewsList = () => {
     const { userId } : UserTypes = useParams();
-    const { data, loading } = useAsync<NewsListI>(() => getNews(userId) , []);
+    const token = getCookieByName('access_token')
+    const { data, loading } = useAsync<NewsListI>(() => getNews(userId, token) , []);
 
     return (
         <div style={{backgroundColor: '#ebedf0', height: "100%", width: "100%",  overflow: "auto"}}>
@@ -37,7 +39,7 @@ const NewsList = () => {
                         </div>
                     }
                     {
-                    !loading && data?.values.map((el) => <New NAME={el.NAME}
+                    !loading && data?.values?.map((el) => <New NAME={el.NAME}
                                                               ID={el.ID}
                                                               PREVIEW_PICTURE={el.PREVIEW_PICTURE}
                                                               PREVIEW_TEXT={el.PREVIEW_TEXT}/> )
